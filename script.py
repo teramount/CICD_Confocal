1670e4db55016e5e21606c4c03b55cf66eae2176
Ricarda Louk <ricarda.louk@teramount.com>
2025-12-30 11:20:03 +0200

    pass args

--- script.py
+++ script.py
@@ -1,1 +1,17 @@
-print("Hello GitHub (PS)")
+import argparse
+
+parser = argparse.ArgumentParser(description="Example script")
+parser.add_argument("--name", default="Richard",help="Your name")
+parser.add_argument("--age", type=int, default=20, help="Your age")
+
+args = parser.parse_args()
+
+print(f"Name: {args.name}")
+print(f"Age: {args.age}")
+
+
+# alternative
+#import sys
+#name = sys.argv[1]
+#age = sys.argv[2]
+#print(name, age)
